b2f10f690836813b214437b14f56cd0ce280dd8d
Daler <isoev9733@gmail.com>
2025-05-27 13:06:29 +0400

    Добавил toast

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection, isDevMode} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {authInterceptor} from "./core/interceptors/auth.interceptor";
 import {JWT_OPTIONS, JwtHelperService} from "@auth0/angular-jwt";
-import { provideServiceWorker } from '@angular/service-worker';
+import {provideServiceWorker} from '@angular/service-worker';
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {provideToastr} from "ngx-toastr";
 
 export function tokenGetter() {
   return localStorage.getItem('access_token');
@@ -13,7 +15,7 @@ export function tokenGetter() {
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     {
@@ -25,8 +27,13 @@ export const appConfig: ApplicationConfig = {
       }
     },
     JwtHelperService, provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }),
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      preventDuplicates: true,
+    }),
   ]
 };
